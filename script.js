bf4556241259ced1f1a363e6082f65a8128817d5
Implode7648 <>
2022-12-22 19:29:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function getUserChoice() {
+  let uc = prompt("enter rock, paper or scissors");
+  if (uc === "rock" || uc === "paper" || uc === "scissors")
+    return uc;
+  else {
+    alert("wrong input");
+    return getUserChoice();
+  }
+}
+
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let ch = Math.floor(Math.random() * 3);
+  return choices[ch];
+}
+
+function playRound(userChoice, computerChoice) {
+  if (userChoice == "rock" && computerChoice == "scissors" ||
+        userChoice == "paper" && computerChoice == "rock" ||
+        userChoice == "scissors" && computerChoice == "paper")
+    return 1;
+  else if (userChoice === computerChoice ||
+        userChoice === computerChoice ||
+        userChoice === computerChoice )
+    return 0;
+  else
+    return -1;
+}
+
+for (let i = 0; i < 5; i++) {
+  let roundRes = playRound(getUserChoice(), getComputerChoice());
+  if (roundRes === 1)
+    console.log("you win!") 
+  else if (roundRes === -1)
+    console.log("you lose");
+  else
+    console.log("it's a tie");
+}
